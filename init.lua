92b6105f345f6a50ca87f104ace5af8e7eebc8ac
FATFATHAO <FATHAODESK@163.com>
2025-12-23 09:05:57 +0800

    Lazy Update

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  -- 设置字体和字号（注意空格需转义）
+  vim.o.guifont = "JetBrainsMono Nerd Font Propo:h14"
+  -- 启用连字功能（如需）
+  vim.g.neovide_ligatures = true
+end
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cpp", "c", "ts", "typescript", "javascript", "js", "go" },
+  callback = function()
+    vim.opt_local.expandtab = true -- 将 Tab 转换为空格
+    vim.opt_local.tabstop = 4 -- Tab 显示为 4 空格
+    vim.opt_local.shiftwidth = 4 -- 自动缩进宽度为 4 空格
+    vim.opt_local.softtabstop = 4 -- 退格键删除 4 空格
+  end,
+})
